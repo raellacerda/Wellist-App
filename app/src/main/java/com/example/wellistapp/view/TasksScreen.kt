304f7dd716765ff44b739d185c6e3d43a24e1117
Raellacerda <israellacerda.gomes.santos@gmail.com>
2025-05-13 09:45:36 -0300

    ~criação da home

---
+++ app/src/main/java/com/example/wellistapp/view/TasksScreen.kt
@@ -0,0 +1,82 @@
+package com.example.wellistapp.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TasksScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                ),
+                title = {
+                    Text("Wellist")
+                }
+            )
+        },
+        bottomBar = {
+            BottomAppBar {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    text = "Aqui ficara a tab bar",
+                )
+            }
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    // aqui fica a navegação pára a tela de criação de tarefa
+                }
+            ) {
+                Icon(
+                    Icons.Default.Add,
+                    contentDescription = "Add"
+                )
+            }
+        }
+    ) { innerPadding ->
+
+        // esse componente sera alterado para LazyColumn quando tivermos os itens
+        Column (
+            modifier = Modifier.padding(innerPadding)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+                Text("Aqui colocaremos os itens")
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun TasksScreenPreview() {
+    TasksScreen()
+}
