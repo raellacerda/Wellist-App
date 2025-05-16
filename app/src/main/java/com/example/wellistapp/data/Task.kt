a1eff76be73c87db9ba39e465bf870d8f0651feb
Raellacerda <israellacerda.gomes.santos@gmail.com>
2025-05-16 11:34:30 -0300

    iniciando configurações do room

--- app/src/main/java/com/example/wellistapp/data/Task.kt
+++ app/src/main/java/com/example/wellistapp/data/Task.kt
@@ -1,10 +1,13 @@
 package com.example.wellistapp.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.Date
 
 
+@Entity
 data class Task (
-    val id : Int,
+    @PrimaryKey(autoGenerate = true) val id : Int,
     val title : String,
     val description: String,
     val priority: Priority,
